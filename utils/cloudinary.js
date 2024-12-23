17df5caa2bb17b391530b98201dbc14cefa30520
ShreySinha02 <shrey8340@gmail.com>
2024-12-23 16:02:36 +0530

    secret fix

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 cloudinary.config({ 
     cloud_name: 'ds8kvp24x', 
     api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
 
